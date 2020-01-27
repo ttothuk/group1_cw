6306549468bc08f020e6cad8fcc40ff41083a164
Tibor Toth <40400971@live.napier.ac.uk>
2020-01-27 22:15:54 +0000

    App built.

---
+++ src/main/java/com/napier/sem/App.java
@@ -0,0 +1,31 @@
+package com.napier.sem;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+        System.out.println("All work and no play makes Jack a dull boy");
+    }
+}
